3ae7edfdfb2713dc51ececcacf60ddc2e92830db
Adrian Aquino <kauan.melo2013@hotmail.com>
2026-09-18 20:11:29 -0300

    feat:controller de login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Autenticação
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+
+
+//DashBoard
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
